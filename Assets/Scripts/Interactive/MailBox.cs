131a8ad376580db523a04937a989f7bf3deeb468
Von-Vam <16686jdj@gmail.com>
2022-08-23 09:51:55 -0400

    Using key to open MailBox

--- Assets/Scripts/Interactive/MailBox.cs
+++ Assets/Scripts/Interactive/MailBox.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class MailBox : Interactive
 {
     private SpriteRenderer spriteRender;
+
+    private BoxCollider2D coll;
+
+    public Sprite openSprite;
+
+    private void Awake()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    protected override void OnClickedAction()
+    {
+        spriteRender.sprite = openSprite;
+    }
 }
