5727fd78927a9f5fe26f4cbd920e9d6be3b4595d
Von-Vam <16686jdj@gmail.com>
2022-08-29 22:51:07 -0400

    Update DialogueUI.cs

--- Assets/Scripts/Dialogue/UI/DialogueUI.cs
+++ Assets/Scripts/Dialogue/UI/DialogueUI.cs
@@ -8,6 +8,16 @@ public class DialogueUI : MonoBehaviour
     public GameObject panel;
     public Text dialogueText;
 
+    private void OnEnable()
+    {
+        EventHandler.ShowDialogueEvent += ShowDialogue;
+    }
+
+    private void OnDisable()
+    {
+        EventHandler.ShowDialogueEvent -= ShowDialogue;
+    }
+
     private void ShowDialogue(string dialogue)
     {
         if (dialogue != string.Empty)
