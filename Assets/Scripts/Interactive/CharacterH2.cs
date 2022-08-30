9cf2751a69c9ee51f28c3a8738f41603ce2d02f3
Von-Vam <16686jdj@gmail.com>
2022-08-29 22:44:29 -0400

    Update CharacterH2.cs

--- Assets/Scripts/Interactive/CharacterH2.cs
+++ Assets/Scripts/Interactive/CharacterH2.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class CharacterH2 : Interactive
 {
-    
+    private DialogueController dialogueController;
+
+    private void Awake()
+    {
+        dialogueController = GetComponent<DialogueController>();
+    }
+
+    public override void EmptyClicked()
+    {
+        if (isDone)
+        {
+            dialogueController.ShowDialogueFinish();
+        }
+        else
+            dialogueController?.ShowDialogueEmpty();
+    }
+
+    protected override void OnClickedAction()
+    {
+        dialogueController.ShowDialogueFinish();
+    }
 }
